858871d43716dba278f91f2a0771e80066cc0300
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-04-25 18:18:57 -0300

    Adicionando método de criptografia de senhas

--- src/main/java/org/example/guilhermezuriel/gestaodevagas/security/SecurityConfig.java
+++ src/main/java/org/example/guilhermezuriel/gestaodevagas/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package org.example.guilhermezuriel.gestaodevagas.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,5 +24,10 @@ public class SecurityConfig {
             }
         });
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
