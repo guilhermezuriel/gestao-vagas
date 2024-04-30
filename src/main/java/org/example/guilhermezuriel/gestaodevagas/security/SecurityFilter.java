d51bc6cdd28ad67ff9c3122b31e3a6f73b227937
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-04-30 09:22:38 -0300

    Capturando o token e validando ele pelo JwtProvider

--- src/main/java/org/example/guilhermezuriel/gestaodevagas/security/SecurityFilter.java
+++ src/main/java/org/example/guilhermezuriel/gestaodevagas/security/SecurityFilter.java
@@ -4,13 +4,26 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.example.guilhermezuriel.gestaodevagas.enums.LevelEnum;
+import org.example.guilhermezuriel.gestaodevagas.providers.JWTProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.logging.Level;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    JWTProvider jwtProvider;
+
 
 
     @Override
@@ -18,7 +31,16 @@ public class SecurityFilter extends OncePerRequestFilter {
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
             String header = request.getHeader("Authorization");
-
+        SecurityContextHolder.getContext().setAuthentication(null);
+            if(header == null) {
+                var  subjectToken = this.jwtProvider.validateToken(header);
+                if(subjectToken.isEmpty()){
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                }
+                request.setAttribute("company_id", subjectToken);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(subjectToken,  null, Collections.emptyList());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
             filterChain.doFilter(request, response);
             //Testando push anydesk
 
