776ced30dfa57473eff2b4d29aa9bb0a842d80b1
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-05-12 16:32:10 -0300

    Refatoração no provedor de token

--- src/main/java/org/example/guilhermezuriel/gestaodevagas/providers/JWTProvider.java
+++ src/main/java/org/example/guilhermezuriel/gestaodevagas/providers/JWTProvider.java
@@ -3,6 +3,7 @@ package org.example.guilhermezuriel.gestaodevagas.providers;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -12,19 +13,18 @@ public class JWTProvider {
     @Value("${security.token.secret}")
     private String secretKey;
 
-    public String validateToken(String token){
+    public DecodedJWT validateToken(String token){
         token = token.replace("Bearer ", "");
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
         try{
-            var subject = JWT.require(algorithm)
+            var tokenDecoded = JWT.require(algorithm)
                 .build()
-                .verify(token)
-                .getSubject();
-            return subject;
+                .verify(token);
+            return tokenDecoded;
         }
         catch (JWTVerificationException ex){
             ex.printStackTrace();
-            return "";
+            return null;
         }
 
     }
