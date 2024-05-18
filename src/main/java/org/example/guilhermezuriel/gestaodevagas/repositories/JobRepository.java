b566a5e6278c200bc7990dcd33feaf8b95632836
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-05-18 12:22:43 -0300

    Implementação do método que a realiza a query de busca pela Description no JobRepository

--- src/main/java/org/example/guilhermezuriel/gestaodevagas/repositories/JobRepository.java
+++ src/main/java/org/example/guilhermezuriel/gestaodevagas/repositories/JobRepository.java
@@ -3,7 +3,9 @@ package org.example.guilhermezuriel.gestaodevagas.repositories;
 import org.example.guilhermezuriel.gestaodevagas.entities.company.JobEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface JobRepository extends JpaRepository<JobEntity, UUID> {
+    List<JobEntity> findAllByDescriptionContaining(String description);
 }
