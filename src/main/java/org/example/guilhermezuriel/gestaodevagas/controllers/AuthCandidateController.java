b4ced0bc479c488fe75f45e808c1a3e47684d9d7
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-05-18 17:37:55 -0300

    Documentação dos controllers de autenticação

--- src/main/java/org/example/guilhermezuriel/gestaodevagas/controllers/AuthCandidateController.java
+++ src/main/java/org/example/guilhermezuriel/gestaodevagas/controllers/AuthCandidateController.java
@@ -1,7 +1,14 @@
 package org.example.guilhermezuriel.gestaodevagas.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.guilhermezuriel.gestaodevagas.service.candidate.CandidateService;
 import org.example.guilhermezuriel.gestaodevagas.service.candidate.dto.AuthCandidateRequestDto;
+import org.example.guilhermezuriel.gestaodevagas.service.candidate.dto.AuthCandidateResponseDto;
+import org.example.guilhermezuriel.gestaodevagas.service.company.dto.AuthCompanyResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/candidate")
+@Tag(name = "Autenticar Candidato")
 public class AuthCandidateController {
     @Autowired
     private CandidateService candidateService;
 
     @PostMapping("/auth")
+    @Operation(summary = "Autenticação", description = "Endpoint responsável por autenticar a empresa")
+    @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = AuthCandidateResponseDto.class)))
     public ResponseEntity<Object> authenticateCandidate(@RequestBody AuthCandidateRequestDto authCandidateRequestDto) {
         try{
             var token = candidateService.authenticate(authCandidateRequestDto);
--- src/main/java/org/example/guilhermezuriel/gestaodevagas/controllers/AuthCompanyController.java
+++ src/main/java/org/example/guilhermezuriel/gestaodevagas/controllers/AuthCompanyController.java
@@ -1,7 +1,13 @@
 package org.example.guilhermezuriel.gestaodevagas.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.guilhermezuriel.gestaodevagas.service.company.CompanyService;
 import org.example.guilhermezuriel.gestaodevagas.service.company.dto.AuthCompanyDto;
+import org.example.guilhermezuriel.gestaodevagas.service.company.dto.AuthCompanyResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,12 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/company")
+@Tag(name = "Autenticar Empresa")
 public class AuthCompanyController {
 
     @Autowired
     private CompanyService companyService;
 
     @PostMapping("/auth")
+    @Operation(summary = "Autenticação", description = "Endpoint responsável por autenticar a empresa")
+    @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = AuthCompanyResponseDto.class)))
     public ResponseEntity<Object> create(@RequestBody AuthCompanyDto authCompanyDto){
         try {
             var result = this.companyService.authCompany(authCompanyDto);
